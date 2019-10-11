b285ba3c5cc25a2750730b768128beb80c61dfaf
John <johnmmoss@hotmail.co.uk>
2019-10-11 22:21:20 +0100

    Removed contact and about pages

--- StatementsTrackerWeb/src/app/app-routing.module.ts
+++ StatementsTrackerWeb/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './user-management/login/login.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
-import { AboutComponent } from './shared/about/about.component';
-import { ContactComponent } from './shared/contact/contact.component';
 import { StatementListComponent } from './statements/statement-list/statement-list.component';
 import { StatementAddComponent } from './statements/statement-add/statement-add.component';
 import { StatementEditComponent } from './statements/statement-edit/statement-edit.component';
@@ -26,8 +24,6 @@ const routes: Routes = [
   //{path: "statements/{0}/payments", component: PaymentListComponent  },
   //{path: "statements/{0}/payments/add", component: PaymentAddComponent  },
   //{path: "statements/{0}/payments/{0}/edit", component: PaymentEditComponent  },
-  {path: "about", component: AboutComponent},
-  {path: "contact", component: ContactComponent},
   {path: "unauthorised", component: UnauthorisedComponent},
   {path: "home", component: HomePageComponent},
   {path: "**", component: HomePageComponent}
--- StatementsTrackerWeb/src/app/app.module.ts
+++ StatementsTrackerWeb/src/app/app.module.ts
@@ -9,8 +9,6 @@ import { StatementEditComponent } from './statements/statement-edit/statement-ed
 import { LoginComponent } from './user-management/login/login.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { HomePageComponent } from './shared/home-page/home-page.component';
-import { AboutComponent } from './shared/about/about.component';
-import { ContactComponent } from './shared/contact/contact.component';
 import { UserService } from './user-management/user.service';
 import { StatementAddComponent } from './statements/statement-add/statement-add.component';
 import { PaymentAddComponent } from './payments/payment-add/payment-add.component';
@@ -29,8 +27,6 @@ import { TitleRowComponent } from './shared/components/title-row/title-row.compo
     StatementEditComponent,
     LoginComponent,
     HomePageComponent,
-    AboutComponent,
-    ContactComponent,
     StatementAddComponent,
     PaymentAddComponent,
     PaymentListComponent,
